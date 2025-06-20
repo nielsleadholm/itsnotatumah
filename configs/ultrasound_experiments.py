64b8857a8765d1f07186505a5f4743d6d1b58284
vclay <vkakerbeck@uos.de>
2025-06-20 12:27:54 +0300

    config updates

--- configs/ultrasound_experiments.py
+++ configs/ultrasound_experiments.py
@@ -172,12 +172,12 @@ json_dataset_ultrasound_experiment["dataset_args"]["env_init_func"] = (
     JSONDatasetUltrasoundEnvironment
 )
 json_dataset_ultrasound_experiment["dataset_args"]["env_init_args"] = {
-    # "data_path": os.path.join(
-    #     os.environ["MONTY_DATA"], "ultrasound_test_set/demo_object_spam/"
-    # ),
     "data_path": os.path.join(
-        os.environ["MONTY_DATA"], "ultrasound_train_set/potted_meat_can_cleaned/"
+        os.environ["MONTY_DATA"], "ultrasound_test_set/demo_object_spam/"
     ),
+    # "data_path": os.path.join(
+    #     os.environ["MONTY_DATA"], "ultrasound_train_set/potted_meat_can_cleaned/"
+    # ),
 }
 
 # For learning we use the DisplacementGraphLM.
